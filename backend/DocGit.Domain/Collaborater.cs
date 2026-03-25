785212734669e19d763647c62afadaee72b025f8
Nouramairy <amairy.nour@gmail.com>
2026-03-25 13:36:47 +0100

    classes

---
+++ backend/DocGit.Domain/Collaborater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocGit.Domain
+{
+    public class Collaborater
+    {
+        public string DocumentId { get; set; }
+        public string ViewerUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
