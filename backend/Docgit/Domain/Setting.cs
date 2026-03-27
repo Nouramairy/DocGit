ee8c0eadff56557fb6f0de309b98dbdf3dd7aee5
Nouramairy <amairy.nour@gmail.com>
2026-03-27 22:45:27 +0100

    domain classes, filesController, program.cs sqlserver config

---
+++ backend/Docgit/Domain/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docgit.Domain
+{
+    public class Setting
+    {
+        public string Key { get; set; }  = string.Empty;
+
+        public string UserId { get; set; } = string.Empty;
+
+        public string Value { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
